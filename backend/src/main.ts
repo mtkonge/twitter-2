12d6a048d65b596069bbf77de30f45cafe0a2e96
MikLz69 <mtkongsted@gmail.com>
2022-07-05 13:34:24 +0200

    everything

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ const main = async () => {
     const app = express(); //create express app
     app.use(cors()); //enables cors
     app.use(express.json()); //enables use of json in requests
-    app.use(express.static(__dirname + '/public'));
+    app.use('/', express.static('./public'));
 
     app.listen(PORT, () => console.log("express hosted on port:", PORT)) //starts the express app with port PORT
 }
